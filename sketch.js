c910d5d26f5a532036503f1f6621b9071ac29f24
hokage0985 <gai.pietro@escola.pr.gov.br>
2024-10-30 08:56:21 -0300

    add arquivo sketch

---
+++ sketch.js
@@ -0,0 +1,31 @@
+let cor;
+let posicaohorizontal;
+let posicaovertical;
+
+function setup() {
+  createCanvas(800, 800);
+  background("white")
+  cor=color(random(0,255), random(0,255), random(0,255));
+  posicaohorizontal=200;
+  posicaovertical=200
+}
+
+function draw() {
+  circle(posicaohorizontal,posicaovertical,50);
+  fill(cor);
+  if(mouseIsPressed){ 
+  if(mouseX > posicaohorizontal){
+    posicaohorizontal=posicaohorizontal+7;
+  }
+   if(mouseX < posicaohorizontal){
+    posicaohorizontal=posicaohorizontal-7;
+  }
+  if(mouseY > posicaovertical){
+    posicaovertical=posicaovertical+7;
+  }
+   if(mouseY < posicaovertical){
+    posicaovertical=posicaovertical-7;
+  }
+     cor=color(random(101,111), random(7,254), random(25,192));
+  }
+}
